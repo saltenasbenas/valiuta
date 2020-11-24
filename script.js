d1af78e207e0f40f9e0a63e592312d6202225ca3
sanebas2 <saltenas.benas@gmail.com>
2020-11-24 09:02:35 +0200

    Pakeistas type is text i number

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-let  gbp, usd, eur;
+let gbp, usd, eur;
 
 gbp = document.getElementById("GBP");
 usd = document.getElementById("USD");
 eur = document.getElementById("EUR");
 button = document.getElementById("button");
-function init()
-{
+
+
+function init() {
     gbpConvert();
     eurConvert();
     usdConvert();
-  
-};
 
-function gbpConvert()
-{
-    usd.value = parseFloat(gbp.value) * 1.33;
-    eur.value = parseFloat(gbp.value) * 1.11;
-  
 };
+function gbpConvert() {
+    usd.value = (gbp.value) * 1.33;
+    eur.value = (gbp.value) * 1.11;
 
-function eurConvert()
-{
-    gbp.value = parseFloat(eur.value) * 0.89;
-    usd.value = parseFloat(eur.value) * 1.19;
-   
 };
+function eurConvert() {
+    gbp.value = (eur.value) * 0.89;
+    usd.value = (eur.value) * 1.19;
 
-function usdConvert()
-{
-    gbp.value = parseFloat(usd.value) * 0.75;
-    eur.value = parseFloat(usd.value) *  0.84;
+};
+function usdConvert() {
+    gbp.value = (usd.value) * 0.75;
+    eur.value = (usd.value) * 0.84;
 };
 
 init();
-
-
 button.addEventListener("click", init()
 );
